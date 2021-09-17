28f2e0a781d757f76eecf156d72d0b4fbecce0b0
chmilrv@gmail.com <chmilrv@gmail.com>
2021-09-17 11:25:00 +0300

    Добавьте файлы проекта.

---
+++ DZ2_Queue_ChmilRV/AppSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace DZ2_Queue_ChmilRV
+{
+	public class AppSettings
+	{
+      public string StorageConnectionString { get; set; }
+
+      public string QueueName { get; set; }
+
+      public AppSettings()
+      {
+         var json = File.ReadAllText("AppSetting.json");
+         var appSettings = JsonDocument.Parse(json, new JsonDocumentOptions { CommentHandling = JsonCommentHandling.Skip });
+         StorageConnectionString = appSettings.RootElement.GetProperty("StorageConnectionString").GetString();
+         QueueName = appSettings.RootElement.GetProperty("QueueName").GetString();
+      }
+
+
+   }
+}
---
+++ DZ2_Queue_ChmilRV/Program.cs
@@ -0,0 +1,94 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DZ2_Queue_ChmilRV
+{
+	class Program
+	{
+
+
+		// We'll need a connection string to your Azure Storage account.
+		static string connectionString = "";
+
+		// Name of the queue we'll send messages to
+		static string queueName = "";
+
+		static QueueClient queueClient;
+
+
+
+
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Работа с очередью\n");
+
+			AppSettings appSettings = new AppSettings();
+			connectionString = appSettings.StorageConnectionString;
+			queueName = appSettings.QueueName;
+
+			// Instantiate a QueueClient which will be used to create and manipulate the queue
+			queueClient = new QueueClient(connectionString, queueName);
+
+
+			Console.WriteLine("Отправка в очередь...");
+			await SendMessageQueue("Тестовое сообщение для проверки очереди");
+
+			Console.WriteLine("\n\nЧтение из очереди...");
+			await ReciveAndDeleteMessageQueue();
+
+			appSettings = null;
+
+
+			Console.ReadKey();
+
+		}
+
+		static async Task ReciveAndDeleteMessageQueue()
+		{
+			int counter = 1;
+			var responceQueue = await queueClient.ReceiveMessagesAsync(maxMessages: 10);
+
+			Console.WriteLine($"Получено сообщений: {responceQueue.Value.Length}");
+			// Get the next messages from the queue
+			foreach (QueueMessage message in responceQueue.Value)
+			{
+				// "Process" the message
+				Console.WriteLine($"Сообщение №{counter++} id - {message.MessageId} : {message.Body}");
+
+				// Let the service know we're finished with the message and
+				// it can be safely deleted.
+				await queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+			}
+		}
+
+		static async Task<bool> SendMessageQueue(string message)
+		{
+			try
+			{
+				// Create the queue
+				await queueClient.CreateIfNotExistsAsync();
+
+				if (queueClient.Exists())
+				{
+					// Send a message to the queue
+					await queueClient.SendMessageAsync(message);
+					Console.WriteLine($"Отправлено: {message}");
+					return true;
+				}
+				else
+				{
+					Console.WriteLine($"Убедитесь что очередь существует и сервис доступен");
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Исключение: {ex.Message}\n\n");
+				Console.WriteLine($"Убедитесь что очередь существует и сервис доступен.");
+			}
+			return false;
+		}
+	}
+}
